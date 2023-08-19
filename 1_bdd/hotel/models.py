4b1bc87307d9c75ac80029dece42128db243cdd5
lucas1farias <fariasluuk@gmail.com>
2023-08-19 01:07:40 -0300

    new django project for managing database subject matters

---
+++ 1_bdd/hotel/models.py
@@ -0,0 +1,24 @@
+
+
+from django.db import models
+
+class Hospede(models.Model):
+    cpf = models.CharField("Cpf", max_length=14)
+    nome = models.CharField("Nome", max_length=20)
+    endereco = models.TextField("Endereço", max_length=100)
+
+class CategoriaApt(models.Model):
+    categoria = models.CharField("Categoria", max_length=7)
+    descricao = models.TextField("Descrição", max_length=100)
+    diaria = models.DecimalField("Valor da diária", max_digits=5, decimal_places=2)
+
+class Apartamento(models.Model):
+    num = models.IntegerField("Número do apt.")
+    id_cat_apt = models.ForeignKey(CategoriaApt, on_delete=models.CASCADE, related_name="apartamento")
+
+class Hospedagem(models.Model):
+    ent = models.DateField("Data de entrada", auto_now_add=True)
+    saida = models.DateField("Data de entrada", auto_now=True)
+    id_cliente = models.ForeignKey(Hospede, on_delete=models.CASCADE, related_name="hospedagem")
+    id_apt = models.ForeignKey(Apartamento, on_delete=models.CASCADE, related_name="hospedagem")
+    id_diaria = models.ForeignKey(CategoriaApt, on_delete=models.CASCADE, related_name="hospedagem")
